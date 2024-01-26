883baa13df2ca4ae57980842b9815e16a8d497de
Daniel Cabrera <drocks2000@gmail.com>
2024-01-26 23:41:32 +0100

    [Rutas] Grupos de rutas: ejemplo

--- src/index.php
+++ src/index.php
@@ -1,6 +1,8 @@
 <?php
 
 use Dansrocks\Slimpoc\Action\WelcomeAction;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
 use Slim\Factory\AppFactory;
 use DI\ContainerBuilder;
 use Psr\Log\LogLevel;
@@ -34,6 +36,26 @@ if (file_exists($cachePath) && is_dir($cachePath) && is_writable($cachePath)) {
 }
 
 $app->get('/', WelcomeAction::class)->setName('bienvenido');
+
+$app->group('/books', function (\Slim\Routing\RouteCollectorProxy $group) {
+
+    $group->get('[/]', function (ServerRequestInterface $request, ResponseInterface $response, array $args) {
+        $response->getBody()->write("List books");
+        return $response;
+    })->setName('books-list');
+
+    $group->get('/search', function (ServerRequestInterface $request, ResponseInterface $response, array $args) {
+        $response->getBody()->write("Search a book");
+        return $response;
+    })->setName('book-search');
+
+    $group->get('/show', function (ServerRequestInterface $request, ResponseInterface $response, array $args) {
+        $response->getBody()->write("Show a book");
+        return $response;
+    })->setName('book-show');
+
+});
+
 $app->get('/{name}', WelcomeAction::class)->setName('bienvenido-fulanito');
 
 $app->run();
