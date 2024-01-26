5a7414c9a34327259b12931cff1fe7cc02ca0851
Daniel Cabrera <drocks2000@gmail.com>
2024-01-26 23:18:38 +0100

    [Rutas] Activar cache de rutas

--- src/index.php
+++ src/index.php
@@ -26,6 +26,11 @@ $containerBuilder->addDefinitions([
 AppFactory::setContainer($containerBuilder->build());
 $app = AppFactory::create();
 
+// enable cache for routing
+$routeCachePath = realpath(__DIR__ . '/../cache/');
+$routeCollector = $app->getRouteCollector();
+$routeCollector->setCacheFile($routeCachePath . '/routes.cache');
+
 $app->get('/', WelcomeAction::class)->setName('bienvenido');
 $app->get('/{name}', WelcomeAction::class)->setName('bienvenido-fulanito');
 
